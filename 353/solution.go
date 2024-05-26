c5576c35fd1ea29f88eec3799899b6cc24ad66c6
John Afolayan <jnafolayan@gmail.com>
2024-05-27 00:03:25 +0100

    354: go solution

---
+++ 353/solution.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+	"strings"
+)
+
+var punctFixMap map[string]string
+
+func init() {
+	punctFixMap = map[string]string{
+		"?": "¿",
+		"!": "¡",
+	}
+}
+
+func main() {
+	expect("Feliz cumpleaños!", "¡Feliz cumpleaños!")
+	expect("Ella ya se graduó de la universidad? ¡No!", "¿Ella ya se graduó de la universidad? ¡No!")
+}
+
+func expect(arg string, expected string) {
+	result := fixInvertedPunc(arg)
+	if result != expected {
+		fmt.Printf("expected fixInvertedPunc(%q) == %q, got %q\n", arg, expected, result)
+	}
+}
+
+type punctIndex struct {
+	idx   int
+	punct string
+	fix   string
+}
+
+func fixInvertedPunc(sentence string) string {
+	// Find all punctuations in the sentence
+	var puncts []*punctIndex
+	for idx, c := range sentence {
+		if _, ok := punctFixMap[string(c)]; !ok {
+			continue
+		}
+		puncts = append(puncts, &punctIndex{
+			idx:   idx,
+			punct: string(c),
+			fix:   punctFixMap[string(c)],
+		})
+	}
+
+	// Sort indices
+	slices.SortFunc(puncts, func(a, b *punctIndex) int {
+		return a.idx - b.idx
+	})
+
+	// Set indexes to be relative to the sentences containing the punctuation.
+	var prevP *punctIndex
+	for _, p := range puncts {
+		if prevP == nil {
+			prevP = p
+			continue
+		}
+		p.idx = p.idx - prevP.idx - 1
+		prevP = p
+	}
+
+	return fixInvertedPuncHelper(sentence, puncts)
+}
+
+func fixInvertedPuncHelper(sentence string, puncts []*punctIndex) string {
+	if len(sentence) == 0 || len(puncts) == 0 {
+		return sentence
+	}
+
+	punct := puncts[0]
+	return addPunctIfNeeded(punct, sentence) +
+		fixInvertedPuncHelper(sentence[punct.idx+1:], puncts[1:])
+}
+
+func addPunctIfNeeded(punc *punctIndex, sentence string) string {
+	trimmed := strings.TrimLeft(sentence, " ")
+	if strings.HasPrefix(trimmed, punc.fix) {
+		return sentence
+	}
+
+	leadingSpace := len(sentence) - len(trimmed)
+	fixed := punc.fix + trimmed[:punc.idx+1+leadingSpace]
+
+	return strings.Repeat(" ", leadingSpace) + fixed
+}
--- 353/solution.js
+++ 353/solution.js
@@ -8,7 +8,6 @@ main();
 function main() {
   expect("Feliz cumpleaños!", "¡Feliz cumpleaños!");
   expect("Ella ya se graduó de la universidad? ¡No!", "¿Ella ya se graduó de la universidad? ¡No!");
-
 }
 
 function expect(testArg, toBe) {
@@ -19,14 +18,29 @@ function expect(testArg, toBe) {
 }
 
 function fixInvertedPunc(sentence) {
-  const puncts = Object.keys(punctFixMap)
-    .map(p => ({
-      index: sentence.indexOf(p), 
-      punct: p,
-      fix: punctFixMap[p]
-    }))
-    .filter(punct => punct.index != -1)
-    .sort((a, b) => a.index - b.index);
+  // Find all punctuations in the sentence
+  let puncts = [];
+  for (let i = 0; i < sentence.length; i++) {
+    if (!punctFixMap.hasOwnProperty(sentence[i])) {
+      continue;
+    }
+    puncts.push({
+      index: i, 
+      punct: sentence[i],
+      fix: punctFixMap[sentence[i]]
+    });
+  }
+
+  puncts = puncts
+    // Sort indices
+    .sort((a, b) => a.index - b.index)
+    // Set indexes to be relative to the sentences containing the punctuation.
+    .map((p, idx, arr) => {
+      if (idx > 0) {
+        p.index -= arr[idx-1].index + 1
+      }
+      return p;
+    });
 
   return _fixInvertedPuncHelper(sentence, puncts);
 }
@@ -47,8 +61,8 @@ function _addPunctIfNeeded(punct, sentence) {
     return sentence;
   }
 
-  const fixed = punct.fix + trimmed.substring(0, punct.index + 1);
   const leadingSpace = sentence.length - trimmed.length;
+  const fixed = punct.fix + trimmed.substring(0, punct.index + 1 + leadingSpace);
 
   return fixed.padStart(leadingSpace, " ");
 }
