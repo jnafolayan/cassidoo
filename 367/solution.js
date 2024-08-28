8207216433cd72dfe98f9334fe3eb33f50419e9d
John Afolayan <jnafolayan@gmail.com>
2024-08-28 13:23:32 +0100

    feat: 367

---
+++ 367/solution.js
@@ -0,0 +1,57 @@
+console.log(equalLettersAndDigits("abc12345"));
+console.log(equalLettersAndDigits("a123b4c"));
+console.log(equalLettersAndDigits("a12bc34"));
+
+function equalLettersAndDigits(s) {
+    const letters = new Set();
+    const digits = new Set();
+    const equalSlices = [];
+
+    for (let i = 0; i < s.length; i++) {
+        letters.clear();
+        digits.clear();
+        const equal = equalLettersAndDigitsFn(s, i, letters, digits);
+        const slices = equal.map((idx) => [i, idx, idx - i]);
+        if (slices.length) {
+            equalSlices.push(...slices);
+        }
+    }
+
+    equalSlices.sort((a, b) => {
+        const diff = b[2] - a[2];
+        if (diff == 0) {
+            return a[0] - b[0];
+        }
+        return diff;
+    });
+
+    if (!equalSlices.length) return "";
+
+    const [[start, end, _]] = equalSlices;
+    return s.substring(start, end + 1);
+}
+
+function equalLettersAndDigitsFn(s, offset, letters, digits) {
+    const equalIndices = [];
+    for (let i = offset; i < s.length; i++) {
+        const c = s[i];
+        if (isLetter(c)) {
+            letters.add(c);
+        } else if (isDigit(c)) {
+            digits.add(c);
+        }
+        if (letters.size == digits.size) {
+            equalIndices.push(i);
+        }
+    }
+    return equalIndices;
+}
+
+function isDigit(c) {
+    return Number.isInteger(+c);
+}
+
+function isLetter(c) {
+    const code = c.charCodeAt(0);
+    return (code >= 65 && code <= 90) || (code >= 97 && code <= 122);
+}
